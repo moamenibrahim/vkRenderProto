c13d6b8822902c9936df1978473b68facb31358f
julamsa <jukkap.lamsa@gmail.com>
2021-11-07 14:57:51 +0200

    CMAKE build and some initial ext libraries

---
+++ model/model.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#include "../include/glm/glm.hpp"
+#include "../include/tinygltf/tiny_gltf.h"
+#include "../include/tinyobj/tiny_obj_loader.h"
+
+#include <vector>
+
+class Model
+{
+public:
+
+};
