2a80ebb8de9e1efa038f370ce7e9be100f90bb1f
julamsa <jukkap.lamsa@gmail.com>
2021-12-31 12:14:47 +0200

    Added some mesh-related placeholders

--- src/model/model.h
+++ src/model/model.h
@@ -1,16 +1,9 @@
 #pragma once
 #include "../include/glm/glm.hpp"
-
-#include "../include/tinygltf/tiny_gltf.h"
-#include "../include/tinyobj/tiny_obj_loader.h"
-
 #include <vector>
 
-class Model
+class Model 
 {
-public:
 
 private:
-	std::vector<glm::vec3> vertices_;
-	std::vector<uint32_t> indices_;
 };
